d8306020ee794de166c2eab0dadb4e0214291972
USER <USER@HP>
2021-02-23 18:42:26 +0800

    new branch created

---
+++ src/GitDemoPackageV2/GitDemoClass1V2.java
@@ -0,0 +1,10 @@
+package GitDemoPackageV2;
+
+public class GitDemoClass1V2 {
+	
+	public void test()
+	{
+		System.out.println("testing again");
+	}
+
+}
--- src/GitDemoPackageV2/GitDemoClassV2.java
+++ src/GitDemoPackageV2/GitDemoClassV2.java
@@ -1,9 +1,12 @@
 package GitDemoPackageV2;
 
-public class GitDemoClassV2 {
+public class GitDemoClassV2 extends GitDemoClass1V2{
 
 	public static void main(String[] args) {
 		System.out.println("testing");
+		GitDemoClass1V2 g = new GitDemoClassV2();
+		g.test();
+		
 
 	}
 
